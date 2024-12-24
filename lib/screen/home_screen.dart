4a5a0b43e40a90355eb1511c3475d5f19f81d426
heosangbeom <luv_3_u@naver.com>
2024-12-24 17:18:10 +0900

    home screen

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:random_number_generator/constant/color.dart';
+import 'dart:math';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> numbers = [
+    123,
+    456,
+    789,
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -20,15 +32,32 @@ class HomeScreen extends StatelessWidget {
               _Header(),
 
               /// 숫자가 있는 곳
-              _Body(),
+              _Body(
+                numbers: numbers,
+              ),
 
               /// 버튼이 있는 곳
-              _Footer(),
+              _Footer(
+                onPressed: generateRandomNumber,
+              ),
             ],
           ),
         ),
       ),
     );
+  }
+  void generateRandomNumber(){
+    final rand = Random();
+    final Set<int> newNumbers = {};
+
+    while(newNumbers.length<3){
+      final randomNumber = rand.nextInt(1000);
+      newNumbers.add(randomNumber);
+    }
+
+    setState(() {
+      numbers = newNumbers.toList();
+    });
   }
 }
 
@@ -61,28 +90,51 @@ class _Header extends StatelessWidget {
 }
 
 class _Body extends StatelessWidget {
-  const _Body({super.key});
+  final List<int> numbers;
+
+  const _Body({
+    required this.numbers,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: Text(
-        '''
-              123
-              456
-              789''',
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: numbers
+            .map((e) => e.toString().split(''))
+            .map(
+              (e) => Row(
+                children: e
+                    .map(
+                      (number) => Image.asset(
+                        'asset/img/$number.png',
+                        width: 50.0,
+                        height: 70.0,
+                      ),
+                    )
+                    .toList(),
+              ),
+            )
+            .toList(),
       ),
     );
   }
 }
 
 class _Footer extends StatelessWidget {
-  const _Footer({super.key});
+  final VoidCallback onPressed;
+
+  const _Footer({
+    required this.onPressed,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
-      onPressed: () {},
+      onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         backgroundColor: redColor,
         foregroundColor: Colors.white,
