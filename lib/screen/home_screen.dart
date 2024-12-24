b8776f019b9895b188ce0e05e35830c752d1a846
heosangbeom <luv_3_u@naver.com>
2024-12-24 16:19:32 +0900

    ui

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,10 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:random_number_generator/constant/color.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: primaryColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              /// 제목과 아이콘 버튼이 있는 곳
+              _Header(),
+
+              /// 숫자가 있는 곳
+              _Body(),
+
+              /// 버튼이 있는 곳
+              _Footer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Header extends StatelessWidget {
+  const _Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          '랜덤숫자 생성기',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 30.0,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        IconButton(
+          color: redColor,
+          onPressed: () {},
+          icon: Icon(
+            Icons.settings,
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  const _Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Text(
+        '''
+              123
+              456
+              789''',
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  const _Footer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        backgroundColor: redColor,
+        foregroundColor: Colors.white,
+      ),
+      child: Text(
+        '생성하기!',
+      ),
+    );
   }
 }
